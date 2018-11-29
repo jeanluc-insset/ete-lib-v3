1296c4d95493e5663d74013f606e7a31f012a8da
Jean-Luc DELEAGE <jldeleage@free.fr>
2018-11-29 11:17:40 +0100

    Add the plugin and modify accordingly the insall.sh script

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 mvn install:install-file -Dfile=ete-lib-impl-3.1-jar-with-dependencies.jar -DgroupId=fr.insset.jeanluc -DartifactId=ete-lib-impl -Dversion=3.1 -Dpackaging=jar
 
+mvn install:install-file -Dfile=ete-lib-impl-3.1-jar-with-dependencies.jar -DgroupId=fr.insset.jeanluc -DartifactId=ete-maven-plugin  -Dversion=3.1 -Dpackaging=maven-plugin
